bee28b7c0185c971e4f563efce0e5d5fd3db6f4f
Justus Magin <keewis@posteo.de>
2025-04-01 22:08:53 +0200

    use the right import

--- python/grid_weights/__init__.py
+++ python/grid_weights/__init__.py
@@ -1,5 +1,5 @@
 from grid_weights.grid_weights import conservative_regridding
-from grid_weights.distributed import conservative_weights
+from grid_weights.conservative import conservative_weights
 
 
 __all__ = ["conservative_regridding", "conservative_weights"]
