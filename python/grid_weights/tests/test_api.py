9b68e524af30e7ba1026f160218f706bcf8f707d
Justus Magin <keewis@posteo.de>
2025-04-12 22:23:49 +0200

    check the `by_variable` constructor

--- python/grid_weights/tests/test_api.py
+++ python/grid_weights/tests/test_api.py
@@ -1,4 +1,5 @@
 import pytest
+import xarray as xr
 
 from grid_weights import api
 
@@ -30,3 +31,41 @@ class TestAlgorithms:
         else:
             with pytest.raises(type(error), match=error.args[0]):
                 api.Algorithms(variables)
+
+    @pytest.mark.parametrize(
+        ["ds", "variables", "default", "variable_kwargs", "expected"],
+        (
+            (
+                xr.Dataset({"a": ("x", [1])}),
+                {"a": "conservative"},
+                None,
+                {},
+                {"a": "conservative"},
+            ),
+            (
+                xr.Dataset({"a": ("x", [1])}),
+                None,
+                None,
+                {"a": "conservative"},
+                {"a": "conservative"},
+            ),
+            (
+                xr.Dataset({"a": ("x", [1])}),
+                None,
+                "conservative",
+                {},
+                {"a": "conservative"},
+            ),
+            (
+                xr.Dataset({"a": ("x", [1]), "b": ("x", [2])}),
+                None,
+                "conservative",
+                {},
+                {"a": "conservative", "b": "conservative"},
+            ),
+        ),
+    )
+    def test_by_variable(self, ds, variables, default, variable_kwargs, expected):
+        actual = api.Algorithms.by_variable(ds, variables, default, **variable_kwargs)
+
+        assert actual.variables == expected
